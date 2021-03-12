ca5a479d8d5acb5df49d85e498e6e72e215f2d56
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-03-12 00:44:52 +0000

    Fix deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,28 +7,21 @@ from brownie import (
 )
 
 
-# weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"  # mainnet
-
-
 def main():
     deployer = accounts.load("deployer")
     balance = deployer.balance()
 
-    weth = deployer.deploy(MockToken, "Wrapped Ether", "WETH", 18)
-    wbtc = deployer.deploy(MockToken, "Wrapped Bitcoin", "WBTC", 8)
-    snx = deployer.deploy(MockToken, "Synthetix Network Token", "SNX", 18)
-
-    feeds = deployer.deploy(ChainlinkFeedsRegistry, weth, publish_source=True)
-    feeds.addUsdFeed(weth, "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e")  # eth
-    feeds.addEthFeed(wbtc, "0x2431452A0010a43878bF198e170F6319Af6d27F4")  # btc
-    feeds.addUsdFeed(snx, "0xE96C4407597CD507002dF88ff6E0008AB41266Ee")  # snx
+    feeds = deployer.deploy(ChainlinkFeedsRegistry, publish_source=True)
+    feeds.addUsdFeed("ETH", "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e")
+    feeds.addEthFeed("BTC", "0x2431452A0010a43878bF198e170F6319Af6d27F4")
+    feeds.addUsdFeed("SNX", "0xE96C4407597CD507002dF88ff6E0008AB41266Ee")
 
     pool = deployer.deploy(CubePool, feeds, publish_source=True)
 
-    pool.addCubeToken(wbtc, 0)
-    pool.addCubeToken(wbtc, 1)
-    pool.addCubeToken(snx, 0)
-    pool.addCubeToken(snx, 1)
+    pool.addCubeToken("BTC", 0)
+    pool.addCubeToken("BTC", 1)
+    pool.addCubeToken("SNX", 0)
+    pool.addCubeToken("SNX", 1)
 
     views = deployer.deploy(CubeViews, publish_source=True)
 
